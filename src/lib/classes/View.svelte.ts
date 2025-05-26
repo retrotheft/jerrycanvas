92121bf0f7bd52b4331983d9588612514f9005fe
Jim Bridger <retrotheft@gmail.com>
2025-05-27 09:43:37 +1000

    separated View logic from View component

--- src/lib/classes/View.svelte.ts
+++ src/lib/classes/View.svelte.ts
@@ -1,7 +1,78 @@
+import { type Scene } from './Scene.svelte.js'
+
+type Coords = {
+   x: number,
+   y: number
+}
+
+type Rect = {
+   width: number,
+   height: number
+}
+
 export class View {
+   public sceneToViewRatio = $state<Rect>({ width: 0, height: 0 })
    public zoom = $state(1)
-   public pan = $state({
-      x: 0,
-      y: 0
-   })
+   public zoomMin = $derived(Math.max(this.sceneToViewRatio.width, this.sceneToViewRatio.height))
+   public zoomMax = $state(2)
+   public zoomDampen = $state(200)
+   public pan = $state<Coords>({ x: 0, y: 0 })
+   public scale = $state(1)
+
+   constructor(
+
+   ) { }
+
+   calculateMaxPan(rect: Rect, scene: Scene) {
+      const scaledWidth = scene.width * this.zoom
+      const scaledHeight = scene.height * this.zoom
+      return { x: scaledWidth - rect.width, y: scaledHeight - rect.height }
+   }
+
+   adjustPan(x: number, y: number, rect: Rect, scene: Scene) {
+      const maxPan = this.calculateMaxPan(rect, scene)
+      const p = {
+         x: Math.min(maxPan.x, Math.max(0, this.pan.x -= x)),
+         y: Math.min(maxPan.y, Math.max(0, this.pan.y -= y))
+      }
+      this.pan = p
+   }
+
+   adjustZoom(deltaY: number, rect: Rect, scene: Scene) {
+      const oldZoom = this.zoom
+      this.zoom = Math.min(this.zoomMax, Math.max(this.zoomMin, this.zoom + deltaY / this.zoomDampen))
+      const scaleRatio = this.zoom / oldZoom
+
+      // could potentially move this into setPan or adjustPan with some tweaking
+      const maxPan = this.calculateMaxPan(rect, scene)
+
+      const p = {
+         x: Math.min(maxPan.x, Math.max(0, this.pan.x * scaleRatio)),
+         y: Math.min(maxPan.y, Math.max(0, this.pan.y * scaleRatio))
+      }
+      this.pan = p
+   }
+
+   updateScale(element: HTMLElement, { width, height }: Rect, scene: Scene ) {
+      const scaleX = width / scene.width;
+      const scaleY = height / scene.height;
+      this.scale = Math.max(1, Math.min(scaleX, scaleY))
+
+      const maxPan = this.calculateMaxPan({ width, height }, scene)
+
+      const p = {
+         x: Math.min(maxPan.x, Math.max(0, this.pan.x)),
+         y: Math.min(maxPan.y, Math.max(0, this.pan.y))
+      };
+
+      this.pan = p
+
+      this.sceneToViewRatio = {
+         width: scaleX,
+         height: scaleY
+      }
+
+      if (this.zoom < this.zoomMin) this.zoom = this.zoomMin
+      if (this.zoom > this.zoomMax) this.zoom = this.zoomMax
+   }
 }
--- src/lib/components/View.svelte
+++ src/lib/components/View.svelte
@@ -1,44 +1,32 @@
 <script lang="ts">
    import { type Scene } from "$lib/classes/Scene.svelte.js";
+   import { View } from "../classes/View.svelte.js"
    import { onMount, type Snippet } from 'svelte'
 
-   let { scene, children }: { scene: Scene, children?: Snippet } = $props()
+   let { scene, children, debug }: { scene: Scene, children?: Snippet, debug?: Snippet } = $props()
+
+   const view = new View()
 
    let isReady = $state(false)
    let observer = $state.raw<ResizeObserver>()
    let viewElement = $state<HTMLDivElement>()
 
    let isMouseDown = $state(false)
-   let zoom = $state(1)
-   const ZOOM_DAMPEN = 200
-   // need to prevent zoom below 1 unless view is smaller
-   // first, need to calculate allowed ratio for x and y
-   //
-   // could zoom out on resize in these cases
-   const ZOOM_MIN = 1
-   const ZOOM_MAX = 2
-
-   let sceneToViewRatio = $state.raw({
-      width: 0,
-      height: 0
-   })
-
-   let scale = $state(1)
-   let pan = $state.raw({
-      x: 0,
-      y: 0
-   })
 
    let lastMouse = $state.raw({
       x: 0,
       y: 0
    })
+
    const setLastMouse = (x: number, y: number) => lastMouse = { x, y }
-
-   const onwheel = (event: WheelEvent) => adjustZoom(event.deltaY)
-
    const onmouseup = (e: MouseEvent) => isMouseDown = false
    const onmouseleave = (e: MouseEvent ) => {}
+
+   function onwheel (event: WheelEvent) {
+      const rect = viewElement?.getBoundingClientRect()
+      if (!rect) return
+      view.adjustZoom(event.deltaY, rect, scene)
+   }
 
    function onmousedown(event: MouseEvent) {
       setLastMouse(event.clientX, event.clientY)
@@ -53,63 +41,15 @@
       const deltaY = event.clientY - lastMouse.y;
 
       setLastMouse(event.clientX, event.clientY)
-      adjustPan(deltaX, deltaY, rect)
+      view.adjustPan(deltaX, deltaY, rect, scene)
    }
 
-
-   function updateScale(element: HTMLElement, { width, height }: { width: number, height: number} ) {
-      const scaleX = width / scene.width;
-      const scaleY = height / scene.height;
-      scale = Math.max(1, Math.min(scaleX, scaleY))
-
-      const maxPan = calculateMaxPan({ width, height })
-
-      pan = {
-         x: Math.min(maxPan.x, Math.max(0, pan.x)),
-         y: Math.min(maxPan.y, Math.max(0, pan.y))
-      };
-
-      // update sceneToViewRatio
-
-
-   }
-
-   function calculateMaxPan(rect: { width: number, height: number}) {
-      const scaledWidth = scene.width * zoom
-      const scaledHeight = scene.height * zoom
-      return { x: scaledWidth - rect.width, y: scaledHeight - rect.height }
-   }
-
-   // could potentially move pan custom property into CSS
-   function adjustPan(x: number, y: number, rect: DOMRect) {
-      const maxPan = calculateMaxPan(rect)
-      const p = {
-         x: Math.min(maxPan.x, Math.max(0, pan.x -= x)),
-         y: Math.min(maxPan.y, Math.max(0, pan.y -= y))
-      }
-      pan = p
-   }
-
-   function adjustZoom(deltaY: number) {
-      const oldZoom = zoom
-      zoom = Math.min(ZOOM_MAX, Math.max(ZOOM_MIN, zoom + deltaY / ZOOM_DAMPEN))
-      const scaleRatio = zoom / oldZoom
-      const rect = viewElement!.getBoundingClientRect()
-
-      // could potentially move this into setPan or adjustPan with some tweaking
-      const maxPan = calculateMaxPan(rect)
-
-      pan = {
-         x: Math.min(maxPan.x, Math.max(0, pan.x * scaleRatio)),
-         y: Math.min(maxPan.y, Math.max(0, pan.y * scaleRatio))
-      }
-   }
    function setup(element: HTMLDivElement) {
       viewElement = element
       if (typeof window !== 'undefined') {
          observer = new ResizeObserver(entries => {
             const entry = entries[0]
-            if (entry.target instanceof HTMLElement) updateScale(entry.target, entry.contentRect)
+            if (entry.target instanceof HTMLElement) view.updateScale(entry.target, entry.contentRect, scene)
          })
          observer.observe(element)
       }
@@ -119,10 +59,10 @@
       // untrack(() => updateScale(element, element.getBoundingClientRect()))
       element.style.setProperty('max-width', scene.width + 'px')
       element.style.setProperty('max-height', scene.height + 'px')
-      element.style.setProperty('--pan-x', -pan.x + 'px')
-      element.style.setProperty('--pan-y', -pan.y + 'px')
-      element.style.setProperty('--zoom', zoom.toString())
-      element.style.setProperty('--scale', `${zoom * scale}`)
+      element.style.setProperty('--pan-x', -view.pan.x + 'px')
+      element.style.setProperty('--pan-y', -view.pan.y + 'px')
+      element.style.setProperty('--zoom', view.zoom.toString())
+      element.style.setProperty('--scale', `${view.zoom * view.scale}`)
    }
 
    onMount(() => {
@@ -134,6 +74,7 @@
 
 <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
 <div role="application" id="view" use:setup {@attach setStyles} {onmousemove} {onmouseup} {onmousedown} {onwheel} {onmouseleave}>
+   {@render debug?.()}
    {#if isReady}
       <scene.svelte>
          {@render children?.()}
