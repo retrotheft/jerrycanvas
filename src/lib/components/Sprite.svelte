edbc5ffabaa0fb4f9c3636e0b68bfe50dd6cd83e
Jim Bridger <retrotheft@gmail.com>
2025-05-27 11:12:54 +1000

    added onclick

--- src/lib/components/Sprite.svelte
+++ src/lib/components/Sprite.svelte
@@ -3,12 +3,13 @@
    import { style } from '../functions/style.js'
    import { type Snippet } from 'svelte'
 
-   let { src, children, ...rest }: { src: string, children?: Snippet, [key: string]: any } = $props()
+   let { src, onclick: onclickExternal, children, ...rest }: { src: string, onclick?: (event: MouseEvent) => void, children?: Snippet, [key: string]: any } = $props()
 
    const onmousedown = (e: MouseEvent) => e.stopPropagation()
+   const onclick = (event: MouseEvent) => onclickExternal?.(event)
 </script>
 
-<button class="sprite" {@attach style(rest)} {onmousedown}>
+<button class="jc-sprite" {@attach style(rest)} {onmousedown} {onclick}>
    <img {src} alt={src} />
    {@render children?.()}
 </button>
